64bbfb20fc73e9e507cb48cc369c5c0cb2118608
Bruno Andreotti <bandreotti@proton.me>
2024-04-23 14:37:18 -0300

    Make default indentation style 4 spaces

--- helix-view/src/document.rs
+++ helix-view/src/document.rs
@@ -54,7 +54,7 @@ use crate::{
 /// 8kB of buffer space for encoding and decoding `Rope`s.
 const BUF_SIZE: usize = 8192;
 
-const DEFAULT_INDENT: IndentStyle = IndentStyle::Tabs;
+const DEFAULT_INDENT: IndentStyle = IndentStyle::Spaces(4);
 const DEFAULT_TAB_WIDTH: usize = 4;
 
 pub const DEFAULT_LANGUAGE_NAME: &str = "text";
